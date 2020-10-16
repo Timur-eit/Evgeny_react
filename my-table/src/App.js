c4623aaee94c25cd76657d060dbc92cfca183660
greyewi1 <greyewi@gmail.com>
2020-10-16 13:00:36 +0300

    add table component

--- my-table/src/App.js
+++ my-table/src/App.js
@@ -1,41 +1,20 @@
-import React, {useState, useCallback} from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Table from './components/table'
 
 function App() {
-  const [iterator, setIterator] = useState(0)
-  const [count, setCount] = useState(0)
+  const [tableData, setTableData] = useState([])
 
-  // const handleChangeIterator = () => {
-  //   setIterator(iterator => {
-  //     setCount(count => count + 1)
-  //     return iterator + 1
-  //   })
-  // }
-
-  const handleChangeIterator = useCallback(() => {
-    setIterator(iterator + 1)
-    setCount(count + 1)
-  }, [iterator, count])
-
-  console.log("render")
+  useEffect(() => {
+    fetch('https://gist.githubusercontent.com/Greyewi/b6da020196da66028c3058ea0746a08f/raw/7bdb672721f7526844e137ad41f1f1bf31df4e0a/Evgeny_table.json')
+      .then(response => response.json())
+      .then(data => setTableData(data))
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <span onClick={handleChangeIterator}>{iterator}{count}</span>
+        <Table data={tableData}/>
       </header>
     </div>
   );
---
+++ my-table/src/components/table/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Table = ({data}) => {
+  return(
+    <table border="1">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>class</th>
+          <th>author</th>
+          <th>current version</th>
+        </tr>
+      </thead>
+      <tbody>{
+        data.map((item, i) => <tr key={i}>{
+          Object.keys(item).map((cell, key) => <td key={key}>{item[cell]}</td>)
+        }</tr>)
+      }</tbody>
+    </table>
+  )
+}
+
+export default Table
