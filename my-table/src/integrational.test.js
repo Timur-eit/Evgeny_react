2d52918dfb46acd1ced48a0c5a4b4250d592c707
Evgeny <evtsyga@gmail.com>
2021-03-29 21:36:56 +0300

    update user test

--- my-table/src/integrational.test.js
+++ my-table/src/integrational.test.js
@@ -29,10 +29,10 @@ describe.skip('check endpoints', () => {
       })
       .catch(function ({response}) {
         console.log(response)
-        const regexp = new RegExp(/(Endpoint not found (\d))/)
+        // const regexp = new RegExp(/(Endpoint not found (\d))/)
         // const errorMessage = JSON.parse(JSON.stringify(error)).message
-        const errorMessage = error.message
-        const errorStatusCodeGroup = +errorMessage.match(regexp)[2]
+        // const errorMessage = error.message
+        // const errorStatusCodeGroup = +errorMessage.match(regexp)[2]
         
         expect(errorStatusCodeGroup).toBe(4)
 
--- my-table/src/user.test.js
+++ my-table/src/user.test.js
@@ -31,14 +31,31 @@ describe("smoke tests", () => {
 
   test('main logo has found', async () => {
     await page.goto('http://localhost:3000/')
-    const html = await page.$eval('.App > a', target => target.innerHTML)
-    await expect(html).toBe('Языкы программирования')
+    const links = await page.$$eval('a', links => links.map(item => item.id));
+    // console.log(links)
+    // const html = await page.$eval('a', target => target.innerHTML)
+    expect(links.includes('prog-languages')).toBe(true)
   }, 15000)
 
   test('main logo has found on page /table', async () => {
     await page.goto('http://localhost:3000')
-    await page.click('.App > a')
-    const html = await page.$eval('.App > a', target => target.innerHTML)
-    await expect(html).toBe('Языкы программирования')
+    await page.click('a#prog-languages')
+    const html = await page.$eval('.App > a#prog-languages', target => target.innerHTML)
+    expect(html).toBe('Языкы программирования')
   }, 25000)
+
+  test('form with 4 fields was found on page /table', async () => {
+    await page.goto('http://localhost:3000')
+    await page.click('#prog-languages')
+    const form = await page.$('form')
+    const fieldNum = form ? await page.$eval('form', elem => {
+      return Array.from(elem.children)
+        .map(x => x.className)
+        .filter(x => x === 'field').
+        length
+     }) : null
+    expect(form).toBeTruthy()
+    expect(fieldNum).toBe(4)
+
+  }, 15000)
 })
